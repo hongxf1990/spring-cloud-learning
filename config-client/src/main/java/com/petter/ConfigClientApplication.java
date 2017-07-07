691567600e15707e148c1c1310b292e3b89cbdf3
hongxf1990 <hongxf19900420@gmail.com>
2017-07-07 11:25:57 +0800

    构建config-server，连接到git仓库 在git上创建config-repo目录，存储配置信息 构建config-client，获取git中的配置信息

---
+++ config-client/src/main/java/com/petter/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.petter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hongxf
+ * @since 2017-07-07 11:13
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
