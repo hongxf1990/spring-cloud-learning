d0b847e942476a03809438e67acd7deeebe4c041
hongxf1990 <hongxf19900420@gmail.com>
2017-07-07 12:14:48 +0800

    修改了配置信息，测试实现配置刷新3

--- config-client/src/main/java/com/petter/ConfigClientApplication.java
+++ config-client/src/main/java/com/petter/ConfigClientApplication.java
@@ -2,11 +2,13 @@ package com.petter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author hongxf
  * @since 2017-07-07 11:13
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
--- config-server/src/main/java/com.petter/ConfigServerApplication.java
+++ config-server/src/main/java/com.petter/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.petter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author hongxf
  * @since 2017-07-07 10:00
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
