b332f050587d15cc7e8601fbedf64f5f0952f634
hongxf1990 <hongxf19900420@gmail.com>
2017-07-21 18:14:35 +0800

    修改config-client的一些配置

--- config-client/src/main/java/com/petter/web/TestController.java
+++ config-client/src/main/java/com/petter/web/TestController.java
@@ -1,7 +1,6 @@
 package com.petter.web;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hongxf
  * @since 2017-07-07 11:17
  */
-@RefreshScope
 @RestController
 public class TestController {
 
